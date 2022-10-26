f8a92a471405d528f5eccf5106471f3dac3a4bb2
Tommy <tcm257@yahoo.com>
2022-10-26 12:59:27 -0500

    Lab 16

--- projects/hands-on-react/src/components/ProjectsPage/ProjectForm.tsx
+++ projects/hands-on-react/src/components/ProjectsPage/ProjectForm.tsx
@@ -6,6 +6,10 @@ interface ExtendedHTMLInputElement extends HTMLInputElement {
     type: HTMLInputTypeAttribute;
 }
 
+type ProjectErrors = {
+    [key in keyof Pick<Project, "name" | "description" | "budget">]: string;
+};
+
 interface ProjectFormProps {
     project: Project;
     onSave: (project: Project) => void;
@@ -15,9 +19,15 @@ interface ProjectFormProps {
 export default function ProjectForm({ project: initialProject, onSave, onCancel }: ProjectFormProps): JSX.Element
 {
     const [project, setProject] = useState(initialProject);
+    const [errors, setErrors] = useState<ProjectErrors>({
+        name: "",
+        description: "",
+        budget: "",
+    });
     
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
+        if(!isValid()) return;
         onSave(project);
     };
 
@@ -40,16 +50,54 @@ export default function ProjectForm({ project: initialProject, onSave, onCancel 
 
             return updatedProject;
         });
+
+        setErrors(() => validate(updatedProject));
     };
+
+    const validate = (project: Project) => {
+        let errors: ProjectErrors = { name: "", description: "", budget: "" };
+
+        if(project.name.length === 0)
+            errors.name = "'Name' is required.";
+        if(project.name.length > 0 && project.name.length < 3)
+            errors.name = "'Name' must be at least 3 characters long.";
+        if(project.description.length === 0)
+            errors.description = "'Description' is required.";
+        if(project.budget === 0)
+            errors.budget = "'Budget' must be greater than 0.";
+        
+        return errors;
+    };
+
+    const isValid = () => (
+        errors.name.length === 0 &&
+        errors.description.length === 0 &&
+        errors.budget.length === 0
+    );
 
     return (
         <form className={`${ styles.ProjectForm } input-group vertical`} onSubmit={handleSubmit}>
             <label htmlFor="name">Project Name</label>
             <input type="text" name="name" placeholder="Name" value={ project.name } onChange={handleChange} />
+            {errors.name.length > 0 && (
+                <div className="card error">
+                    <p>{ errors.name }</p>
+                </div>
+            )}
             <label htmlFor="description">Project Description</label>
             <textarea name="description" placeholder="Description" value={ project.description } onChange={handleChange} />
+            {errors.description.length > 0 && (
+                <div className="card error">
+                    <p>{ errors.description }</p>
+                </div>
+            )}
             <label htmlFor="budget">Project Budget</label>
             <input type="number" name="budget" placeholder="Budget" value={ project.budget } onChange={handleChange} />
+            {errors.budget.length > 0 && (
+                <div className="card error">
+                    <p>{ errors.budget }</p>
+                </div>
+            )}
             <label htmlFor="isActive">Active?</label>
             <input type="checkbox" name="isActive" checked={ project.isActive } onChange={handleChange} />
             <div className="input-group">
